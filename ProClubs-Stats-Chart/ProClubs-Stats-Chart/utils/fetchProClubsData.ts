46f3216aa278b1730b469277e5fcd47c93c13eb4
Drewmaloney <158063108+Drewmaloney@users.noreply.github.com>
2024-12-29 15:54:51 -0500

    Delete ProClubs-Stats-Chart/ProClubs-Stats-Chart directory

--- ProClubs-Stats-Chart/ProClubs-Stats-Chart/utils/fetchProClubsData.ts
+++
@@ -1,8 +0,0 @@
-export async function fetchProClubsData() {
-  const response = await fetch('https://proclubs.ea.com/api/nhl/members/stats?platform=common-gen5&clubId=34385')
-  if (!response.ok) {
-    throw new Error('Failed to fetch data')
-  }
-  return response.json()
-}
-
